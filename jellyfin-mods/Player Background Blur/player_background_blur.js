608d844f1ef39f1f000b7cb71382ab4bcbba3512
manri <alexmm119@gmail.com>
2025-04-06 13:41:31 +0200

    CHANGE: fix update of Jellyfin 10.10.7

--- jellyfin-mods/Player Background Blur/player_background_blur.js
+++ jellyfin-mods/Player Background Blur/player_background_blur.js
@@ -1,35 +1,37 @@
-    function loadBlur() {
+function loadBlur() {
         const imgElement2 = document.getElementsByClassName('nowPlayingImage')[0].attributes[1].nodeValue;
         const imgElement3 = imgElement2.replace('background-image: ', '');
         const imgElement = imgElement3.replace(';', '');
-        
+
+
         if (imgElement === 'display: none') {
-            console.log("Album cover is null");
+                console.log("Album cover is null");
         } else {
-            var styles = `
-                .backgroundContainer {
-                background: --var(wallpaper) center top no-repeat #033361;
-                -webkit-background-size: cover;
-                background-size: cover;
-                background-position: center !important;
-                backdrop-filter: saturate(0.5) blur(80px) brightness(0) !important;
-                -webkit-filter: saturate(0.5) blur(80px) brightness(0) !important;
+                console.log(imgElement);
+                var styles = `
+                    .backgroundContainer {
+                      background: --var(wallpaper) center top no-repeat #033361;
+                      -webkit-background-size: cover;
+                      background-size: cover;
+                      background-position: center !important;
+                      backdrop-filter: saturate(0.5) blur(50px) brightness(-1) !important;
+                      -webkit-filter: blur(50px) !important;
+                  }
+
+                  :root{
+                    --wallpaper: ` + imgElement + ` !important;}
+                `
+                var styleSheet = document.createElement("style")
+                styleSheet.innerText = styles
+                document.head.appendChild(styleSheet)
+
+                return imgElement;
             }
-
-            :root{
-                --wallpaper: ` + imgElement + ` !important;}
-            `
-            var styleSheet = document.createElement("style")
-            styleSheet.innerText = styles
-            document.head.appendChild(styleSheet)
-
-            return imgElement;
         }
-    }
 
-    // load function loadBlur
+    // Call function when page is loaded
     window.onload = loadBlur;
 
-    // Listen for changes in the DOM to update the background if the song changes.
-    const observer = new MutationObserver(loadBlur);i
+    // Lisen change on the DOM  to update background with album image
+    const observer = new MutationObserver(loadBlur);
     observer.observe(document.body, { childList: true, subtree: true });
